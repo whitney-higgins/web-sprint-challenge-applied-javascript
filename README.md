156bd915a3612b09fc0d4ade9275610a5e556f67
whitney-higgins <whitneyxhiggins@gmail.com>
2023-06-06 16:59:10 -0500

    Header appended to the DOM

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ You are not allowed to collaborate during the sprint challenge.
 
 ### Project Set Up
 
-- [ ] Create a forked copy of this project
-- [ ] Clone your OWN version of the repository (Not BloomTech's by mistake!)
+- [x] Create a forked copy of this project
+- [x] Clone your OWN version of the repository (Not BloomTech's by mistake!)
 - [ ] Implement the project on the main branch, committing changes regularly
 - [ ] Push commits: `git push origin main`
 
@@ -27,11 +27,11 @@ In meeting the minimum viable product (MVP) specifications listed below, your pr
 
 ### Instructions
 
-- [ ] Navigate to the root of the project with your command line.
-- [ ] Run `npm install` to download the dependencies listed in the `package.json` file.
-- [ ] Run `npm start` to compile the project and serve it.
-- [ ] Navigate Chrome to `http://localhost:3000`
-- [ ] In a separate terminal, run `npm test` to run tests.
+- [x] Navigate to the root of the project with your command line.
+- [x] Run `npm install` to download the dependencies listed in the `package.json` file.
+- [x] Run `npm start` to compile the project and serve it.
+- [x] Navigate Chrome to `http://localhost:3000`
+- [x] In a separate terminal, run `npm test` to run tests.
 
 **Steps Required for MVP:**
 
--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+
+  // create elements
+  const mainHeader = document.createElement("div");
+  const pageDate = document.createElement("span");
+  const titleHeader = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  // give elements context
+  pageDate.textContent = date;
+  titleHeader.textContent = title;
+  temperature.textContent = temp;
+
+  mainHeader.classList.add("header");
+  pageDate.classList.add("date");
+  temperature.classList.add("temp");
+
+  // create hiearchy
+  mainHeader.appendChild(pageDate);
+  mainHeader.appendChild(titleHeader);
+  mainHeader.appendChild(temperature);
+
+  return mainHeader;
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,12 +42,18 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-
-  // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
+  // HINT: querySelector can take in a string (ie querySelector("#wrapper"))
   // but it can also take in a variable (ie querySelector(selector))
   // We are taking care of passing in the correct selector on line 16,
   // so all that you need to do is pass it into the querySelector method
   // for the tests to work!
-}
 
-export { Header, headerAppender }
+  const cssSelect = document.querySelector(selector);
+  cssSelect.appendChild(
+    Header("Breaking News...", "June 6, 2023", "77 degrees")
+  );
+
+  return cssSelect;
+};
+
+export { Header, headerAppender };
